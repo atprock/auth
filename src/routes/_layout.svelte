bbc6cca25ee4818c63e1388e3ccde343a7c81034
EC2 Default User <ec2-user@ip-172-31-60-35.ec2.internal>
2021-03-17 12:25:32 +0000

    Initial layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,26 +1,22 @@
-<script context="module">
-	export const preload = () => {};
-</script>
-
 <script>
-	import { stores } from "@sapper/app";
-
-	// You may not want to use `segment`, but it is passed for the time being and will
-	// create a warning if not expected: https://github.com/sveltejs/sapper-template/issues/210
-	// https://github.com/sveltejs/sapper/issues/824
-	export let segment = "";
-	// Silence unused export property warning
-	// eslint-disable-next-line no-empty
-	if (segment) {}
-
-	const { page } = stores();
-	$: path = $page.path.slice(1);
+  // Importing modules
+  import { onMount } from "svelte";
+  
+  let loaded = false;
+  
+  onMount(() => {
+    setTimeout(() => loaded = true, 1000);
+  });
 </script>
 
-<svelte:head>
-	<title>
-		{path ? path.charAt(0).toUpperCase() + path.slice(1) : "Index"}
-	</title>
-</svelte:head>
-
-<slot />
+<!-- Loading screen  -->
+{ #if !loaded }
+  <div style="z-index: 999;" class="fixed w-full h-screen flex justify-center items-center bg-white">
+    <p>Loading</p>
+  </div>
+<!-- Layout -->
+{ :else }
+  <main>
+    <slot />
+  </main>
+{ /if }
